7bb2c672503593ca6f92899ddddd87dba7c321a4
jepra777 <jere.prasetyo.jp@gmail.com>
2022-05-08 09:54:37 +0700

    Combine FE & BE on Register and Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,10 +2,44 @@ import Navbar from "../components/Navbar"
 import { Flex, Center, Text, Box, Heading, Input, InputGroup, InputRightElement, InputLeftElement, Button } from "@chakra-ui/react"
 import { AtSignIcon, LockIcon, ChevronRightIcon } from "@chakra-ui/icons"
 import React from "react"
+import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
 
 export default function Login() {
     const [show, setShow] = React.useState(false)
     const handleClick = () => setShow(!show)
+    const navigate = useNavigate()
+    const formik = useFormik({
+        initialValues:{
+          Username: "",
+          Password: ""
+        },
+        onSubmit: (values) => {
+            fetch('http://localhost:3001/v1/login', {
+                method: 'POST',
+                headers: { 
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json' },
+                credentials: "include",
+                body: JSON.stringify(values),
+            })
+            .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+            console.log(data, 'This is the Data')
+            if (data.message === "Success"){
+                localStorage.setItem('id', data.id);
+                localStorage.setItem('role', data.Role);
+                localStorage.setItem('access_token', data.Access_token);
+                navigate("/")
+            }
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+        }
+      })
     return (
         <>
         {/* Navbar - Start */}
@@ -21,6 +55,7 @@ export default function Login() {
             px={8}
             mb={16}
         >
+            <form onSubmit={formik.handleSubmit}>
             <Center>
                 <Box as='article' maxW='sm' p='5' borderWidth='1px' rounded='md'>
                     <Heading size='md' my='2'>
@@ -36,7 +71,14 @@ export default function Login() {
                     pointerEvents='none'
                     children={<AtSignIcon color='gray.300' />}
                     />
-                    <Input variant='outline' placeholder='Username or Email' />
+                    <Input 
+                        variant='outline' 
+                        placeholder='Username or Email'
+                        id='Username'
+                        name='Username'
+                        value={formik.values.Username}
+                        onChange={formik.handleChange}
+                    />
                     </InputGroup>
                     
                     <InputGroup size='md' my='4'>
@@ -48,6 +90,10 @@ export default function Login() {
                         pr='4.5rem'
                         type={show ? 'text' : 'password'}
                         placeholder='Enter password'
+                        id='Password'
+                        name='Password'
+                        value={formik.values.Password}
+                        onChange={formik.handleChange}
                     />
                     <InputRightElement width='4.5rem'>
                         <Button h='1.75rem' size='sm' onClick={handleClick}>
@@ -56,11 +102,16 @@ export default function Login() {
                     </InputRightElement>
                     </InputGroup>
                     
-                    <Button colorScheme='gray' leftIcon={<ChevronRightIcon />}>
+                    <Button 
+                        colorScheme='gray' 
+                        leftIcon={<ChevronRightIcon />}
+                        type="submit"
+                    >
                         Log In
                     </Button>
                 </Box>
             </Center>
+            </form>
         </Flex>
         {/* Log In Content - End */}
         </>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,10 +2,39 @@ import Navbar from "../components/Navbar"
 import { Flex, Center, Text, Box, Heading, Input, InputGroup, InputRightElement, InputLeftElement, Button } from "@chakra-ui/react"
 import { EmailIcon, AtSignIcon, LockIcon, ChevronRightIcon } from "@chakra-ui/icons"
 import React from "react"
+import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
 
 export default function Register() {
     const [show, setShow] = React.useState(false)
     const handleClick = () => setShow(!show)
+    const navigate = useNavigate()
+    const formik = useFormik({
+        initialValues:{
+            Username: '',
+            Email: '',
+            Password: ''
+        },
+        onSubmit: values => {
+            fetch('http://localhost:3001/v1/register', {
+                method: 'POST',
+                headers: { 
+                    'Content-Type': 'application/json' },
+                credentials: "include",
+                body: JSON.stringify(values),
+            })
+            .then((response) => {
+            return response.json();
+            })
+            .then(() => {
+            navigate("/login");
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+        },
+    })
+
     return (
         <>
         {/* Navbar - Start */}
@@ -21,6 +50,7 @@ export default function Register() {
             px={8}
             mb={16}
         >
+            <form onSubmit={formik.handleSubmit}>
             <Center>
                 <Box as='article' maxW='sm' p='5' borderWidth='1px' rounded='md'>
                     <Heading size='md' my='2'>
@@ -36,7 +66,14 @@ export default function Register() {
                     pointerEvents='none'
                     children={<AtSignIcon color='gray.300' />}
                     />
-                    <Input variant='outline' placeholder='Username' />
+                    <Input 
+                        variant='outline' 
+                        placeholder='Username' 
+                        id='Username'
+                        name='Username'
+                        value={formik.values.Username}
+                        onChange={formik.handleChange}
+                    />
                     </InputGroup>
 
                     <InputGroup size='md' my='4'>
@@ -44,7 +81,14 @@ export default function Register() {
                     pointerEvents='none'
                     children={<EmailIcon color='gray.300' />}
                     />
-                    <Input variant='outline' placeholder='Email' />
+                    <Input 
+                        variant='outline' 
+                        placeholder='Email' 
+                        id='Email'
+                        name='Email'
+                        value={formik.values.Email}
+                        onChange={formik.handleChange}
+                    />
                     </InputGroup>
                     
                     <InputGroup size='md' my='4'>
@@ -56,6 +100,10 @@ export default function Register() {
                         pr='4.5rem'
                         type={show ? 'text' : 'password'}
                         placeholder='Enter password'
+                        id='Password'
+                        name='Password'
+                        value={formik.values.Password}
+                        onChange={formik.handleChange}
                     />
                     <InputRightElement width='4.5rem'>
                         <Button h='1.75rem' size='sm' onClick={handleClick}>
@@ -64,11 +112,16 @@ export default function Register() {
                     </InputRightElement>
                     </InputGroup>
                     
-                    <Button colorScheme='gray' leftIcon={<ChevronRightIcon />}>
+                    <Button 
+                        colorScheme='gray' 
+                        leftIcon={<ChevronRightIcon />}
+                        type="submit"
+                    >
                         Join
                     </Button>
                 </Box>
             </Center>
+            </form>
         </Flex>
         {/* Register Content - End */}
         </>
